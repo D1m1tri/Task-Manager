fa772a5d260f48fc7ff4ea0da2d174d892640538
D1m1tri <enzosantanna00@gmail.com>
2024-05-20 00:12:59 -0300

    Created tables and task creator relationship

---
+++ app/Models/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'task',
+        'description',
+        'completed',
+        'completed_at',
+        'owner_id',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Task;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Task::create([
+            'task' => 'Test Task',
+            'description' => 'This is a test task.',
+            'completed' => false,
+            'completed_at' => null,
+            'owner_id' => 1,
         ]);
     }
 }
